e28420405a86e0a142c617b2dc7e0d3f090a77e9
siva2482 <sivaranjani.palaniyappan@gmail.com>
2022-12-11 22:30:39 -0600

    delete task

--- src/test/java/Pages/TaskPage.java
+++ src/test/java/Pages/TaskPage.java
@@ -1,5 +1,8 @@
 package Pages;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -30,12 +33,27 @@ public class TaskPage extends TestBase{
 		}
 	 public void enterTask()
 	 {
-		 TitleBox.sendKeys("SmokeTesting");
-		 TitleText.sendKeys("TO check the critical functionalities ofthe system");
+		 TitleBox.sendKeys("UnitTesting");
+		 TitleText.sendKeys("TO check each functionalities ofthe system");
 		 TitleDate.sendKeys("12/02/2021");
 		 Taskbutton.click();
 		 
 	 }
+	 public void deleteTask()
+	 {
+		 //driver.findElement(By.xpath("(//div//child::div[@class='card w-100']//input[@type='file'])[1]")).click();
+		// driver.findElement(By.xpath("(//div//child::div[@class='card w-100']//input[@type='file'])[1]")).sendKeys("D://input.txt");
+		 //driver.findElement(By.xpath("(//button[contains(text(),'Upload')])[1]")).click();
+		 List<WebElement> tasks=driver.findElements(By.xpath("//div//child::h5"));
+		 for(int i=0;i<tasks.size();i++)
+		 {
+		 if(tasks.get(i).getText().equalsIgnoreCase("Sanity testing"))
+		 {
+			 driver.findElement(By.xpath("//div//button[contains(text(),'Delete')]")).click();
+		 }
+		 
+	 }
 	
 
+}
 }
---
+++ src/test/java/Test/UploadTest.java
@@ -0,0 +1,25 @@
+package Test;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import Base.TestBase;
+import Pages.TaskPage;
+
+public class UploadTest extends TestBase{
+	
+TaskPage tobj=null;
+	
+	@BeforeTest
+	public void init()
+	{
+		tobj=new TaskPage(driver);
+		
+	}
+	@Test
+	public void run()
+	{
+		tobj.deleteTask();
+	}
+
+}
